5e6dbd58f412a60c652e9279a8a3133a1a3ff0da
manofoar <joetynan@gmail.com>
2019-08-05 19:51:36 +0000

    All tests passing

--- gists_database/search.py
+++ gists_database/search.py
@@ -1,4 +1,26 @@
 from .models import Gist
+from datetime import datetime
+import sqlite3
+
 
 def search_gists(db_connection, **kwargs):
-    pass
+    '''
+    for kwarg,value in kwargs.items():
+        if isinstance(value,datetime):
+            blah = db_connection.execute("SELECT * FROM gists WHERE datetime({}) == datetime(:{})"".format(kwarg,kwarg))
+        else:
+            blah = db_connection.execute("SELECT * FROM gists WHERE {} = :{}".format(kwarg,kwarg))
+        result = [Gist(gist) for gist in blah]
+    return result
+'''
+    query = '''
+        SELECT * FROM gists '''
+
+    for kwarg, value in kwargs.items():
+        if isinstance(value, datetime):
+            query = query + 'WHERE datetime({}) == datetime(:{})'.format(kwarg, kwarg)
+        else:
+            query = query + 'WHERE {} = :{}'.format(kwarg, kwarg)
+    cursor = db_connection.execute(query, kwargs)
+    results = [Gist(gist) for gist in cursor]
+    return results
